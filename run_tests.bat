c22151ff8f3c11587bfaf1fe19cc91e9b6cfe06e
Manikanta Jalla <manikanta.jalla8@gmail.com>
2024-08-07 13:09:07 -0400

    modified bat file

--- run_tests.bat
+++ run_tests.bat
@@ -1,5 +1,14 @@
 @echo off
 
+:: Check if the browser argument is provided
+if "%1"=="" (
+    echo Usage: %0 browser
+    exit /b 1
+)
+
+:: Set the browser variable from the command-line argument
+set BROWSER=%1
+
 :: Get the current timestamp
 set TIMESTAMP=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 
@@ -10,4 +19,4 @@ set TIMESTAMP=%TIMESTAMP: =0%
 set OUTPUT_DIR=results\results_%TIMESTAMP%
 
 :: Run Robot Framework tests with the dynamic output directory
-robot -d %OUTPUT_DIR% tests/EndToEnd/End_to_End.robot
+robot -d %OUTPUT_DIR% -v BROWSER:%BROWSER% tests/EndToEnd/End_to_End.robot
